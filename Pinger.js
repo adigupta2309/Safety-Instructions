650fef8b6ec66c16baa116266a9ddcaa7a28a709
adigupta2309 <91068256+adigupta2309@users.noreply.github.com>
2021-09-21 15:31:48 +0530

    show Info Popup added and committed to new branch

--- Pinger.js
+++ Pinger.js
@@ -10,3 +10,7 @@ $timeout(function(){
   $scope.view.wdg['pinger5'].shader = "pinger; rings f 5; rate f 4; direction f -1; r f 0.4; g f 0.5; b f 0.6"; 
   $scope.view.wdg['pinger6'].shader = "pinger; rings f 5; rate f 4; direction f -1; r f 0.4; g f 0.5; b f 0.6"; 
 },1000);
+
+$scope.showPopup = function(){
+  $scope.view.wdg['infoPopup']['visible'] = true;
+};
