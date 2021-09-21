d11de39bdbb47c2a7eae8fc7ddc8172905192352
adigupta2309 <91068256+adigupta2309@users.noreply.github.com>
2021-09-21 14:57:25 +0530

    Created pinger.js

---
+++ Pinger.js
@@ -0,0 +1,12 @@
+console.log($scope.app)
+
+//timeout function
+
+$timeout(function(){
+  $scope.view.wdg['pinger1'].shader = "pinger; rings f 5; rate f 4; direction f -1; r f 0.4; g f 0.5; b f 0.6"; 
+  $scope.view.wdg['pinger2'].shader = "pinger; rings f 5; rate f 4; direction f -1; r f 0.4; g f 0.5; b f 0.6"; 
+  $scope.view.wdg['pinger3'].shader = "pinger; rings f 5; rate f 4; direction f -1; r f 0.4; g f 0.5; b f 0.6";
+  $scope.view.wdg['pinger4'].shader = "pinger; rings f 5; rate f 4; direction f -1; r f 0.4; g f 0.5; b f 0.6"; 
+  $scope.view.wdg['pinger5'].shader = "pinger; rings f 5; rate f 4; direction f -1; r f 0.4; g f 0.5; b f 0.6"; 
+  $scope.view.wdg['pinger6'].shader = "pinger; rings f 5; rate f 4; direction f -1; r f 0.4; g f 0.5; b f 0.6"; 
+},1000);
